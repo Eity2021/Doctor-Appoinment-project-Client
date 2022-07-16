061618e1548cdf10ad11a8940d4eb266d67b4f9a
Eity2021 <Eity2021akter@gmail.com>
2022-07-16 13:27:33 +0600

    tailwind css fully setup

--- src/Doctor/Home/Home.js
+++ src/Doctor/Home/Home.js
@@ -4,6 +4,9 @@ const Home = () => {
   return (
     <div>
       <h1>this is home page page</h1>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 };
