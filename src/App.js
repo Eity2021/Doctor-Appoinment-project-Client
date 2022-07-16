ab4103d3a504b473f6db59c426abaf5be8d3053d
Eity2021 <Eity2021akter@gmail.com>
2022-07-16 13:20:02 +0600

    complete router setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Doctor/Home/Home';
+import About from './Doctor/About/About';
+import Appointment from './Doctor/Appointment/Appointment';
+import Reviews from './Doctor/Reviews/Reviews';
+import ContactUs from './Doctor/ContactUs/ContactUs';
+import Login from './Doctor/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="about" element={<About />} />
+        <Route path="appointment" element={<Appointment />} />
+        <Route path="reviews" element={<Reviews />} />
+        <Route path="contactUs" element={<ContactUs />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Doctor/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <link to="/">Home</link>
+            <link to="/about">About</link>
+            <link to="/appointment">Appointment</link>
+            <link to="/reviews">Reviews</link>
+            <link to="/contactUs">Contact Us</link>
+            <link to="/login">Login</link>
+
+        </div>
+    );
+};
+
+export default Header;
